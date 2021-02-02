0c66505d44e24016716a9f3c8ac1caae7daa041d
Janusz Lenar <malleor@gmail.com>
2021-02-02 22:28:34 +0100

    use the baseline blurhash.ts to decode a hash

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
+import { decode } from "blurhash";
+import $ from 'jquery';
 
-function component() {
-  const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+const decode_groundtruth = function(blur_hash, width, height, canvas) {
+  const pixels = decode(blur_hash, width, height);
+  const ctx = canvas.getContext("2d");
+  const imageData = ctx.createImageData(width, height);
+  imageData.data.set(pixels);
+  ctx.putImageData(imageData, 0, 0);
 }
 
-document.body.appendChild(component());
+const width=400, height=400;
+var canvas_gt = $("#ground_truth");
+decode_groundtruth("LFDv4iofM|M{E8%MM{of0LWCoxoy", width, height, canvas_gt.get(0));
