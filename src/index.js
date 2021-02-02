7d1d093548a963f84c555ea8c18841fe2bdb8aed
Janusz Lenar <malleor@gmail.com>
2021-02-02 22:45:11 +0100

    placeholder for vanilla js output

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import { decode } from "blurhash";
 import $ from 'jquery';
 
 
-const decode_groundtruth = function(blur_hash, width, height, canvas) {
+const decode_groundtruth = function(blur_hash, canvas_id) {
+  var canvas = $(canvas_id);
+  const width=canvas.attr("width"), height=canvas.attr("height");
   const pixels = decode(blur_hash, width, height);
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.get(0).getContext("2d");
   const imageData = ctx.createImageData(width, height);
   imageData.data.set(pixels);
   ctx.putImageData(imageData, 0, 0);
-}
+};
 
-const width=400, height=400;
-var canvas_gt = $("#ground_truth");
-decode_groundtruth("LFDv4iofM|M{E8%MM{of0LWCoxoy", width, height, canvas_gt.get(0));
+const decode_vanilla = function(blur_hash, canvas_id) {
+};
+
+decode_groundtruth("LFDv4iofM|M{E8%MM{of0LWCoxoy", "#output_gt");
+decode_vanilla("LFDv4iofM|M{E8%MM{of0LWCoxoy", "#output_vanilla");
